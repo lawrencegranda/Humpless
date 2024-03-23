b95de042b1cc1a309975df1140f654d8e32ce4e5
Lawrence Granda Zarzuela <lawrencegranda@gmail.com>
2024-03-23 15:44:36 -0400

    Write from_csv function
    - Load csv using CSV library

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,10 @@
-let lines = Csv.load "test.csv"
+let from_csv path =
+  let lines = Csv.load path in
+  let headers, rows =
+    match lines with
+    | [] -> ([], [])
+    | h :: t -> (h, t)
+  in
+  (headers, rows)
 
-let headers, rows = 
+let _ = from_csv "test.csv"
