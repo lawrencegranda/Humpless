68d4a813359ce7c7e5821f0063fb2d96e084b54c
lsf58 <lsf58@cornell.edu>
2024-03-23 16:27:08 -0400

    Create a test for Table_ops

--- test/test_final_project.ml
+++ test/test_final_project.ml
@@ -1,8 +1,8 @@
 open OUnit2
+open Final_project.Table_ops
 
 let tests =
   [
-    ("empty csv" >:: fun _ -> assert_equal [] (from_csv "empty.csv"));
-    ( "not empty csv" >:: fun _ ->
-      assert_equal (List.length > 0) (from_csv "test2.csv") );
+    ("empty csv" >:: fun _ -> assert_equal ([], []) (from_csv "empty.csv"));
+    ("not empty csv" >:: fun _ -> assert_equal (from_csv "test2.csv"));
   ]
