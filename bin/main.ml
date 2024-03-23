b658a0d67ba49441b0ef81671f83fecbfb76de11
lsf58 <lsf58@cornell.edu>
2024-03-23 16:00:21 -0400

    Create functionality to create string from the rows

--- bin/main.ml
+++ bin/main.ml
@@ -7,4 +7,17 @@ let from_csv path =
   in
   (headers, rows)
 
-let _ = from_csv "test.csv"
+let headers, rows = from_csv "test.csv"
+
+let list_as_string headers =
+  String.trim (List.fold_left (fun acc h -> acc ^ "\t" ^ h) "" headers)
+
+let row_as_string rows i = list_as_string (List.nth rows i)
+
+let rows_as_string rows =
+  String.trim
+    (List.fold_left (fun acc row -> acc ^ "\n" ^ list_as_string row) "" rows)
+
+let _ = print_endline (list_as_string headers)
+let _ = print_endline (row_as_string rows 0)
+let _ = print_endline (rows_as_string rows)
