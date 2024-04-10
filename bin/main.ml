51e69e31e9667038adccbb55b2f4e2619a06f0c9
js3696@cornell.edu <js3696@cornell.edu>
2024-04-10 13:41:27 -0400

    Enable use of custom csvs

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,12 @@
 open Final_project
 
+(**[csv] is the csv the user passes in. If they omit a file, the default
+   test.csv will be used*)
+let csv = if Array.length Sys.argv > 1 then Sys.argv.(1) else "test.csv"
+
 let () =
   let open Table_ops in
-  let headers, rows = from_csv "test.csv" in
+  let headers, rows = from_csv ("data/" ^ csv) in
   print_endline (list_as_string headers);
   print_endline (row_as_string rows 0);
   print_endline (rows_as_string rows)
