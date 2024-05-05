828274ca2921e3abd9f738cadf2f81c56e3b36e1
js3696@cornell.edu <js3696@cornell.edu>
2024-05-05 14:56:13 -0400

    Update main for basic i/o - main continuously listens for user commands and exits on ctrl c or "exit"

--- bin/main.ml
+++ bin/main.ml
@@ -1,36 +1,34 @@
 open Final_project
-
-let print_styles output style1 style2 =
-  ANSITerminal.print_string [ ANSITerminal.on_default; style1; style2 ] output
-
-let print_question output =
-  print_styles (output ^ "\t") ANSITerminal.Reset ANSITerminal.cyan
-
-(* Print [message] using the [print_question] format and return the input. *)
-let get_input message =
-  let () = print_question message in
-  let output = read_line () in
-  String.trim output
-
-let print_csv path =
-  let open Table_ops in
-  let headers, rows = from_csv path in
-  print_endline (list_as_string headers);
-  print_endline (row_as_string rows 0);
-  print_endline (rows_as_string rows)
+open Table
 
 let path =
   "data/" ^ if Array.length Sys.argv > 1 then Sys.argv.(1) else "test.csv"
+(* let table = make_table path let () = print_table table *)
 
-let () =
-  print_csv path;
-  print_endline "We are going to add a new task."
+(*main operations: - add task - edit task - remove task - filter task *)
 
-let task_name = get_input "Task Name:"
-let description = get_input "Description:"
-let date = get_input "Due Date [YYYY-MM-DD]:"
-let category = get_input "Task Category:"
+let run_command () =
+  try
+    let tab = Sys.argv.(1) in
+    (*handles repetitive (recusive) manipulations to tab*)
+    let process_table_ops () =
+      let rec read_command _ =
+        let%lwt message = Lwt_io.read_line_opt Lwt_io.stdin in
+        match message with
+        | None ->
+            print_endline "Sorry, no command detected";
+            exit 0
+        | Some m ->
+            if m = "exit" then exit 0;
+            let%lwt () = Lwt_io.printl ("Command " ^ m ^ " received") in
+            read_command ()
+      in
+      let _ = read_command () in
+      let (never_resolved : unit Lwt.t), _unused_resolver = Lwt.wait () in
+      never_resolved
+    in
 
-let () =
-  Table_ops.add_task task_name description date category path;
-  print_csv path
+    Lwt_main.run (process_table_ops ())
+  with _ -> print_endline "Please provide a table you'd like to work with."
+
+let _ = run_command ()
