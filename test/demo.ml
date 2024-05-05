e631b8d2faae5c3b3d7a6b58efc73eed7fccdd81
Lawrence Granda Zarzuela <lg626@cornell.edu>
2024-05-05 15:08:18 -0400

    Add to the demo.
    - Add more tasks.

--- test/demo.ml
+++ test/demo.ml
@@ -11,6 +11,30 @@ let print_my_table table =
 let table = make_table "test1.csv" ~autosave:false;;
 
 print_my_table table;;
+remove_task table 0;;
+remove_task table 0;;
+remove_task table 0;;
+remove_task table 0;;
+remove_task table 0;;
+print_my_table table;;
+
+add_task table "3110 Meeting" "  Meet with Sophia" "2024-04-10" "00:00:00"
+  "College" "TODO"
+;;
+
+add_task table "3110 MS2" "type report" "2024-03-28" "" "College" "in-progress"
+;;
+
+add_task table "3110 prelim" "lectures 9-18" "2024-03-22" "11:30:00" "Tests"
+  "done"
+;;
+
+add_task table "Get gas" "for the car" "2024-03-23" "20:00:00" "Peersonal"
+  "todo"
+;;
+
+add_task table "Math homework" "" "2024-03-25" "11:00:40" "College" "TODO";;
+print_my_table table;;
 filter_by_name table "3110";;
 print_my_table table;;
 filter_by_description table "car ";;
